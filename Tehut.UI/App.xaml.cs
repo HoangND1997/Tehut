9aa7bf5d9dee97d44aa79ad6aa7e70a052bef145
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-01-18 23:46:49 +0100

    Added basic window with correct dependency injection

--- Tehut.UI/App.xaml.cs
+++ Tehut.UI/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 using System.Windows;
+using Tehut.Core;
+using Tehut.Database;
+using Tehut.UI.Views;
 
 namespace Tehut.UI
 {
@@ -9,6 +12,30 @@ namespace Tehut.UI
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider; 
+
+        public App()
+        {
+            var databasePath = Path.Combine(Path.Combine(Path.GetTempPath(), "Tehut"), "quiz.db"); 
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddTehutApplication(); 
+            serviceCollection.AddTehutDatabase(new DatabaseConfig { DatabasePath = databasePath });
+
+            serviceCollection.AddSingleton(s => new MainWindow()); 
+
+            serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+
+            mainWindow.Show(); 
+        }
     }
 
 }
