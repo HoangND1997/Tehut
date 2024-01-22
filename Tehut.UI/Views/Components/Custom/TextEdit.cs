5d15d9cad729c6bc23d669d516c9ddb30187a54e
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-01-22 22:18:01 +0100

    Added navigation header with search bar

---
+++ Tehut.UI/Views/Components/Custom/TextEdit.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Tehut.UI.Views.Components.Custom
+{
+    [TemplatePart(Name = "PART_TextBox", Type = typeof(TextBox))]
+    [TemplatePart(Name = "PART_Hint", Type = typeof(TextBlock))]
+    public class TextEdit : TextBox
+    {
+        public string Hint
+        {
+            get => (string)GetValue(HintProperty);
+            set { SetValue(HintProperty, value); }
+        }
+
+        public SolidColorBrush HintColor
+        {
+            get { return (SolidColorBrush)GetValue(HintColorProperty); }
+            set { SetValue(HintColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty = DependencyProperty.Register("Hint", typeof(string), typeof(TextEdit), new PropertyMetadata(""));
+        public static readonly DependencyProperty HintColorProperty = DependencyProperty.Register("HintColor", typeof(SolidColorBrush), typeof(TextEdit), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        private TextBox? textBox;
+        private TextBlock? hint; 
+
+        static TextEdit()
+        { 
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextEdit), new FrameworkPropertyMetadata(typeof(TextEdit)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            GotFocus += TextEdit_GotFocus;
+            LostFocus += TextEdit_LostFocus;
+
+            textBox = Template.FindName("PART_TextBox", this) as TextBox;
+            hint = Template.FindName("PART_Hint", this) as TextBlock;
+
+            if (textBox != null)
+            {
+                textBox.GotFocus += TextEdit_GotFocus; 
+                textBox.LostFocus += TextEdit_LostFocus;
+            }
+        }
+
+        private void TextEdit_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox?.Text) && hint != null)
+            {
+                hint.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void TextEdit_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (hint != null)
+            {
+                hint.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
