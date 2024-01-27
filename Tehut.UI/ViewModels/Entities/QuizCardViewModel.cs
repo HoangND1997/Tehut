b13745938eae4d9e19dac3c84e56d8b215a0885b
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-01-27 09:42:33 +0100

    Add basic quiz overview page structure

---
+++ Tehut.UI/ViewModels/Entities/QuizCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Tehut.Core.Models;
+
+namespace Tehut.UI.ViewModels.Entities
+{
+    public class QuizCardViewModel
+    {
+        public string Name { get; }
+
+        public int QuestionCount { get; }
+
+        public QuizCardViewModel(Quiz quiz) 
+        {
+            Name = quiz.Name;
+            QuestionCount = 18; 
+        }
+    }
+}
--- Tehut.UI/ViewModels/QuizOverviewViewModel.cs
+++ Tehut.UI/ViewModels/QuizOverviewViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using DevExpress.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Tehut.Core.Models;
+using Tehut.UI.ViewModels.Entities;
 
 namespace Tehut.UI.ViewModels
 {
     public class QuizOverviewViewModel : ViewModelBase
     {
+        public ObservableCollection<QuizCardViewModel> Quizzes { get; } = new();
+
+        public QuizOverviewViewModel()
+        {
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 0, Name = "Egyptian Gods" })); 
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 1, Name = "Greek Gods" })); 
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 2, Name = "Roman Gods" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 3, Name = "U.S. Presidents" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 4, Name = "Basic Chemistry" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 5, Name = "German History" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 6, Name = "Swiss Mountains" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 7, Name = "Football" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 8, Name = "Coding" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 9, Name = "World Capitals" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 10, Name = "Animal Kingdom" }));
+            Quizzes.Add(new QuizCardViewModel(new Quiz { Id = 11, Name = "Architecture" }));
+        }
     }
 }
