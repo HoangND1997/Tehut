23aaec42e0bcb6bc9fb8cf443a2767483c48c81a
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-02-02 22:56:49 +0100

    Added correct labels for the amount of questions in a quiz

---
+++ Tehut.UI/Views/Converters/QuestionCountConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Tehut.UI.Views.Converters
+{
+    internal class QuestionCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int questionCount)
+            {
+                if (questionCount == 0)
+                {
+                    return "No Questions";
+                }
+                else if (questionCount == 1)
+                {
+                    return "1 Question";
+                }
+                else 
+                {
+                    return $"{questionCount} Questions";
+                }
+            }
+
+            return string.Empty; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value; 
+        }
+    }
+}
